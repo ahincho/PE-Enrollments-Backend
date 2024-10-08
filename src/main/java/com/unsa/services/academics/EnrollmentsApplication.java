e40a8a29966fa0506b97314f050087537b954bb5
ahincho <ahincho@unsa.edu.pe>
2024-10-08 10:29:07 -0500

    Start Spring Project

---
+++ src/main/java/com/unsa/services/academics/EnrollmentsApplication.java
@@ -0,0 +1,11 @@
+package com.unsa.services.academics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/unsa/services/academics/EnrollmentsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.unsa.services.academics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentsApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
